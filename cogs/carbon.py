aa0bf65c1149f5df76715e744c7a331e1398bc6e
USPRO <konfigmeykir@gmail.com>
2020-10-05 18:52:52 +0300

    Carbon Modülündeki Bug Düzeltildi

--- cogs/carbon.py
+++ cogs/carbon.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 import os
 import requests
-
+import urllib.parse
 
 class Carbon(commands.Cog):
 
@@ -11,11 +11,12 @@ class Carbon(commands.Cog):
 
 	@commands.command()
 	async def carbon(self, ctx, *, kod):
+		urlkod = urllib.parse.quote_plus(kod)
 		if os.path.exists("Carbon-Kod.jpg"):
 			os.remove("Carbon-Kod.jpg")
 		if len(kod) < 1:
 			await ctx.send('Kullanım "/carbon kod" Şeklindedir')
-		req = requests.get(f"https://carbonnowsh.herokuapp.com/?code={kod}")
+		req = requests.get(f"https://carbonnowsh.herokuapp.com/?code={urlkod}")
 
 		with open("Carbon-Kod.jpg", 'wb') as f:
 			f.write(req.content)
