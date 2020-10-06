c659ff4c9323d36086a58d8f52d995eb0a22418e
USPRO <konfigmeykir@gmail.com>
2020-10-06 14:19:33 +0300

    Deneme123

---
+++ cogs/istatistik.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+
+class İstatistik(commands.Cog):
+    
+    def __init__(self, client):
+        self.client = client
+        
+    @commands.command(aliases=["sistatistik"])
+    async def sunucuistatistik(self, ctx):
+            await ctx.send(guild.name)
+        
+        
+        
+        
+def setup(client):
+    client.add_cog(İstatistik(client))
