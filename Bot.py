bfcd8d8bea480dca40963ade5470440b33969909
USPRO <konfigmeykir@gmail.com>
2020-10-12 16:14:34 +0300

    Beceremedim .d

--- Bot.py
+++ Bot.py
@@ -8,7 +8,7 @@ import os
 async def on_ready():
 	print("Ready")
 	
-plugins = ["cogs.HitLoggerChecker", "cogs.Ban", "cogs.Unban", "cogs.Kick", "cogs.OtoRol", "cogs.Clear", "cogs.Report", "cogs.Yardım", "cogs.term", "cogs.covid19", "cogs.carbon", "cogs.antikufur"]
+plugins = ["cogs.HitLoggerChecker", "cogs.Ban", "cogs.Unban", "cogs.Kick", "cogs.OtoRol", "cogs.Clear", "cogs.Report", "cogs.Yardım", "cogs.term", "cogs.covid19", "cogs.carbon"]
 
 for plugin in plugins:
 	client.load_extension(plugin)
@@ -26,11 +26,6 @@ async def on_command_error(ctx, error):
 async def on_member_join(member):
 	giris_cikis_kanali = client.get_channel(int(os.environ.get("GIRIS_CIKIS_ID")))
 	await giris_cikis_kanali.send(f"{member.mention} Sunucuya Katıldı Hoş Geldin, Rolün Verildi")
-@client.event
-async def on_message(message):
-        with open("küfürler.txt") as f:
-            for i in f:
-                if i in message.content:
-                    await message.author.ban()    
+
 
 client.run(os.environ.get("DISCORD_TOKEN"))
