a2a592a9170e5da6f144a392d257cfa96de6fa90
USPRO <konfigmeykir@gmail.com>
2020-10-15 15:16:48 +0300

    Deneme...

--- cogs/Unban.py
+++ cogs/Unban.py
@@ -1,29 +1,21 @@
 import discord
 from discord.ext import commands
 import os
-
+ 
 class Unban(commands.Cog):
 
 	def __init__(self, client):
 		self.client = client
 
 	@commands.command()
+	@commands.guild_only()
 	@commands.has_permissions(ban_members=True)
-	async def unban(self, ctx, *, member: discord.Member):
-		banned_users = await ctx.guild.bans()
-		member_name, member_discriminator = member.split("#")
+	async def unban(self, ctx, userID):
+		user = discord.Object(id=userID)
+		await ctx.guild.unban(user)
+        
 
-		for ban_entry in banned_users:
-			user = ban_entry.user
-
-			if (user.name, user.desciminator) == (member_name, member_discriminator):
-				await ctx.guild.unban(user)
-				await ctx.send(f"{user.mention}'ın Banı Kaldırıldı")
-				log = self.client.get_channel(int(os.environ.get("LOG_ID")))
-				await log.send(f"{member} İçin Ban Kaldırıldı")
-				await ctx.send(f"{member} İçin Ban Kaldırıldı")
-				return
-
+		
 
 def setup(client):
 	client.add_cog(Unban(client))
