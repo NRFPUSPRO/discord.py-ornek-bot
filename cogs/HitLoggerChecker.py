15c4dae8ae16f7aef4a28a988701e06ea18cc440
USPRO <konfigmeykir@gmail.com>
2020-10-15 14:37:54 +0300

    Fix...

--- cogs/HitLoggerChecker.py
+++ cogs/HitLoggerChecker.py
@@ -12,22 +12,26 @@ class HitLoggerChecker(commands.Cog):
 	@commands.Cog.listener()
 	async def on_message(self, message):
 		#if message.channel.id == 755772621181943879:
-		config_url = message.attachments[0].url
-		config = requests.get(config_url)
-		configi_kaydet = open(f"./{message.attachments[0].filename}", "wb").write(config.content)
-		kaydedilen_config = open(f"./{message.attachments[0].filename}", "r")
-		for i in kaydedilen_config.readlines():
-			if "api.telegram.org" in i:
-				await message.author.ban(reason="Hit Loggerlı Config Atmak Yasaktır")
-				kaydedilen_config.close()
-			elif "discordapp.com/api/webhooks" in i:
-				await message.author.ban(reason="Hit Loggerlı Config Atmak Yasaktır")
-				kaydedilen_config.close()
-			elif "docs.google.com" in i:
-				await message.author.ban(reason="Hit Loggerlı Config Atmak Yasaktır")
-				kaydedilen_config.close()
-			else:
-				pass
+		try:
+			config_url = message.attachments[0].url
+			config = requests.get(config_url)
+			configi_kaydet = open(f"./{message.attachments[0].filename}", "wb").write(config.content)
+			kaydedilen_config = open(f"./{message.attachments[0].filename}", "r")
+			for i in kaydedilen_config.readlines():
+				if "api.telegram.org" in i:
+					await message.author.ban(reason="Hit Loggerlı Config Atmak Yasaktır")
+					kaydedilen_config.close()
+				elif "discordapp.com/api/webhooks" in i:
+					await message.author.ban(reason="Hit Loggerlı Config Atmak Yasaktır")
+					kaydedilen_config.close()
+				elif "docs.google.com" in i:
+					await message.author.ban(reason="Hit Loggerlı Config Atmak Yasaktır")
+					kaydedilen_config.close()
+				else:
+					pass
+		except:
+			pass
+		
 	
 		
 
