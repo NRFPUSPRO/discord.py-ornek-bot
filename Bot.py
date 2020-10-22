508011bdc1f742879d88c4531b25ac35a5ba381c
USPRO <usdogu@gmail.com>
2020-10-22 15:20:22 +0300

    Sublime Texti İcat Edenin

--- Bot.py
+++ Bot.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 import requests
-
+import os
 
 intents = discord.Intents().all()
 
--- cogs/info.py
+++ cogs/info.py
@@ -9,7 +9,6 @@ class Info(commands.Cog):
     @commands.command(aliases=["sinfo"])
     @commands.guild_only()
     async def server_info(self, ctx):
-        
         await ctx.send(f"Sunucu Adı : {ctx.guild.name}\nSunucu Açıklaması : {ctx.guild.description}\nSunucu Sahibi : {ctx.guild.owner}\nBoost Sayısı : {ctx.guild.premium_subscription_count}\nBoost Basan Kişiler : {ctx.guild.premium_subscribers}\nÜye Sayısı : {ctx.guild.member_count}")
 
 
