04d36f54f4e63ecf7223b5e9f144230bfd848718
USPRO <usdogu@gmail.com>
2020-10-22 15:53:09 +0300

    .

--- cogs/info.py
+++ cogs/info.py
@@ -9,7 +9,7 @@ class Info(commands.Cog):
     @commands.command(aliases=["sinfo"])
     @commands.guild_only()
     async def server_info(self, ctx):
-        await ctx.send(f"Sunucu Adı : {ctx.guild.name}\nSunucu Açıklaması : {ctx.guild.description}\nSunucu Sahibi : {ctx.guild.owner}\nBoost Sayısı : {ctx.guild.premium_subscription_count}\nBoost Basan Kişiler : {0 if ctx.guild.premium_subscribers = [] else ctx.guild.premium_subscribers}\nÜye Sayısı : {ctx.guild.member_count}")
+        await ctx.send(f"Sunucu Adı : {ctx.guild.name}\nSunucu Açıklaması : {ctx.guild.description}\nSunucu Sahibi : {ctx.guild.owner}\nBoost Sayısı : {ctx.guild.premium_subscription_count}\nBoost Basan Kişiler : {ctx.guild.premium_subscribers}\nÜye Sayısı : {ctx.guild.member_count}")
 
 
 
